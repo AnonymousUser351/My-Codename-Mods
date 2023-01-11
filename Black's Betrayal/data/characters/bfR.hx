2fc3b0c63fa61a6eba8cb99c5fe3806330ba971d
Mimikeeby <86208538+AnonymousUser351@users.noreply.github.com>
2023-01-10 23:32:23 -0600

    organization

--- Black's Betrayal/data/characters/bfR.hx
+++
@@ -1,9 +0,0 @@
-function onGetCamPos(a:PointEvent) {
-    if (animation.curAnim == null) return;
-    switch (animation.curAnim.name) {
-        case "singLEFT": a.x -= 50;
-        case "singDOWN": a.y += 50;
-        case "singUP": a.y -= 50;
-        case "singRIGHT": a.x += 50;
-    }
-}
